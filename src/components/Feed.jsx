c4d898af347615b2ed58e5628c59fd47397f5b4c
myuzara12 <myuzara12@gmail.com>
2023-10-04 21:16:54 +0700

    add Videos layout

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import { Box, Stack, Typography } from '@mui/material'
-import Sidebar from "./Sidebar"
+import { Videos, Sidebar } from './'
+
 
 
 const Feed = () => {
@@ -12,6 +13,20 @@ const Feed = () => {
           Copyright 2023 myuzara
         </Typography>
       </Box>
+
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }} >
+        <Typography
+          variant="h4"
+          sx={{ color: 'white' }}
+          mb={2}
+          fontWeight={'bold'}
+        >
+          New <span style={{ color: '#F31503' }}> Videos </span>
+        </Typography>
+
+        <Videos />
+      </Box>
+
     </Stack>
   )
 }
