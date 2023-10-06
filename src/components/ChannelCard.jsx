a220d0bc86d193fb60d0993f0b3921f2ea66d537
myuzara12 <myuzara12@gmail.com>
2023-10-06 19:48:10 +0700

    add video card & channel card

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,8 +1,63 @@
-import React from 'react'
+import { Box, CardMedia, CardContent, Typography } from "@mui/material"
+import { Link } from "react-router-dom"
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
-const ChannelCard = () => {
+import { demoProfilePicture } from "../utils/constants"
+
+
+const ChannelCard = ({ channelDetail }) => {
   return (
-    <div>ChannelCard</div>
+    <Box
+      sx={{
+        boxShadow: 'none',
+        borderRadius: '20px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: { xs: '356px', md: "320px" },
+        height: '326px',
+        margin: 'auto'
+      }}
+    >
+      <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+        <CardContent
+          sx={{
+            display: "flex",
+            flexDirection: 'column',
+            justifyContent: 'center',
+            textAlign: 'center',
+            color: '#fff',
+          }}
+        >
+          <CardMedia
+            image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture}
+            alt={channelDetail?.snippet?.title}
+            sx={{
+              borderRadius: '50%',
+              height: '180px',
+              width: '180px',
+              mb: 2,
+              border: '1px solid',
+
+            }}
+          />
+
+          <Typography variant="h6">
+            {channelDetail?.snippet?.title}
+            <CheckCircleIcon sx={{ fontSize: "14px", color: "gray", ml: "5px" }} />
+          </Typography>
+
+          {
+            channelDetail?.statistics?.subscriberCount && (
+              <Typography>
+                {parseInt(channelDetail?.statistics?.subscriberCount).toLocaleString()} Subscribers
+              </Typography>
+            )
+          }
+
+        </CardContent>
+      </Link>
+    </Box>
   )
 }
 
